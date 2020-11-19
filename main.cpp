af2101121cabb6c73d3b4b6e173dcfdaef50a03d
vna15 <30414490+vna15@users.noreply.github.com>
2020-11-18 21:10:46 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
    Sculptor draw(100,100,100);
-   draw.setColor(1,1,1,1);
+   draw.setColor(255,255,255,1);
    draw.putEllipsoid(50,50,50,30,20,10);
    draw.writeOFF((char*) "cubo.off");
 
